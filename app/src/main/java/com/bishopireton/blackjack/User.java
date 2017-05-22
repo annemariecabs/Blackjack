6ce1e6f28569372122ec70c40c0380f33162c0c5
annemariecabs <caballeroa1@bishopireton.org>
2017-05-22 14:39:53 -0400

    Newest version of Blackjack

---
+++ app/src/main/java/com/bishopireton/blackjack/User.java
@@ -0,0 +1,28 @@
+package com.bishopireton.blackjack;
+
+import java.util.ArrayList;
+
+/**
+ * Created by AnneMarie on 5/22/2017.
+ */
+
+public class User {
+    private ArrayList<Card> cards;
+
+    User(ArrayList<Card> c) {
+        cards = c;
+    }
+
+    public void addCard(Card c) {
+        cards.add(c);
+    }
+
+    public int sumCards() {
+        int sum = 0;
+        for(Card card: cards)
+            sum += card.getRank();
+
+        return sum;
+    }
+
+}
