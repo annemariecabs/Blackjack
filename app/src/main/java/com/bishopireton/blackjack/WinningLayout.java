0f7e6cc86de735a1b7e3a94842af456cd81b758f
annemariecabs <caballeroa1@bishopireton.org>
2017-06-04 17:25:20 -0400

    6/4

---
+++ app/src/main/java/com/bishopireton/blackjack/WinningLayout.java
@@ -0,0 +1,18 @@
+package com.bishopireton.blackjack;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by AnneMarie on 6/4/2017.
+ */
+
+public class WinningLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.winning_layout);
+
+    }
+}
